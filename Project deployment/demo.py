1b0989047a309ce8f001517a153c8b7371b1d696
Nikita Jadhav <130852909+Mounudh@users.noreply.github.com>
2023-11-07 01:23:28 -0800

    Add files via upload

---
+++ Project deployment/demo.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 16 19:21:32 2023
+
+@author: hp
+"""
+
+import streamlit as st
+
+
+st.title('Machine Learning Model')
+st.title('Prediction of Energy Production For Combined Cycle Power Plant')
+st.markdown('This is Web Application')
+
